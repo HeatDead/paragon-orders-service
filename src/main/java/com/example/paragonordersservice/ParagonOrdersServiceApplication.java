677ae4d316de139cdebcedf9fdf1c8124afbe339
Jotaro <zibble@mail.ru>
2022-07-05 16:41:31 +0400

    init commit

---
+++ src/main/java/com/example/paragonordersservice/ParagonOrdersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.paragonordersservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParagonOrdersServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParagonOrdersServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/paragonordersservice/ParagonOrdersServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paragonordersservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParagonOrdersServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
